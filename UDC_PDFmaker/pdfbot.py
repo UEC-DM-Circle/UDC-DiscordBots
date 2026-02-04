383f6c6d1c57ebbd123178af06b923b0473787f9
Gitudon <oniku.udon.029@gmail.com>
2026-02-04 20:58:01 +0900

    PDFMaker: 修正

--- UDC_PDFmaker/pdfbot.py
+++ UDC_PDFmaker/pdfbot.py
@@ -7,10 +7,6 @@ from pdfgene import generate_pdf_binary
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
-GENERATE = int(os.getenv("GENERATE"))
-TEST = int(os.getenv("TEST"))
-pdf_name = "artifact.pdf"
-
 GENERATE_CHANNEL_ID = int(os.getenv("GENERATE_CHANNEL_ID"))
 TEST_CHANNEL_ID = int(os.getenv("TEST_CHANNEL_ID"))
 PDF_NAME = "artifact.pdf"
@@ -21,10 +17,6 @@ client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 def legal_url(url: str):
     pattern = r"^https:\/\/gachi-matome\.com\/deckrecipe-detail-dm\/\?tcgrevo_deck_maker_deck_id=+"
     return bool(re.match(pattern, url))
-
-
-# Bot定義
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 
 @client.event
@@ -39,19 +31,6 @@ async def test(ctx):
 
 @client.command()
 async def pdfmake(ctx, *args):
-    if ctx.channel.id not in [TEST, GENERATE]:
-        await ctx.send("専用のチャンネルで実行してください")
-        return
-    url = None
-    ngr_option = False
-    nsp_option = False
-    for arg in args:
-        if arg == "-ngr":
-            ngr_option = True
-        elif arg == "-nsp":
-            nsp_option = True
-        elif arg.startswith("http"):
-            url = arg
     if ctx.channel.id not in [TEST_CHANNEL_ID, GENERATE_CHANNEL_ID]:
         await ctx.send("専用のチャンネルで実行してください")
         return
@@ -71,10 +50,6 @@ async def pdfmake(ctx, *args):
         await ctx.send("urlが不正です")
         return
 
-    await ctx.send("生成中です")
-    pdf_binary = generate_pdf_binary(url, ngr_option, nsp_option)
-    await ctx.send(file=discord.File(fp=pdf_binary, filename=pdf_name))
-    await ctx.send(f"{ctx.author.mention} 生成完了しました")
     await ctx.send("生成中です")
     pdf_binary = generate_pdf_binary(url, ngr_option, nsp_option)
     await ctx.send(file=discord.File(fp=pdf_binary, filename=PDF_NAME))
