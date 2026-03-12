5ea175efbeae4712a65bde30f784d8f642a5ff06
Gitudon <oniku.udon.029@gmail.com>
2026-03-13 00:50:31 +0900

    Card-Recruitment: 方針を記入

--- UDC_Card-Recruitment/bot/want.py
+++ UDC_Card-Recruitment/bot/want.py
@@ -2,6 +2,11 @@ from common import *
 from use_mysql import UseMySQL
 
 
+# 以下のメソッドを作る
+# 送るメッセージを作成する
+# SQLを実行する
 class Want:
-    async def parse_args(args: list) -> str:
+    async def parse_args(ctx: commands.Context, args: list) -> str:
         arg_length = len(args)
+        if arg_length == 2:
+            person = ctx.author.display_name
