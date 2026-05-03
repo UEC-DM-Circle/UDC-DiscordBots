e4acbde779fc9fd65b14ea05f982c784d58541fc
Gitudon <oniku.udon.029@gmail.com>
2026-05-03 21:48:04 +0900

    PDFmaker: commonの編集

--- UDC_PDFmaker/common.py
+++ UDC_PDFmaker/common.py
@@ -27,8 +27,9 @@ API_BASE_URL = os.getenv("API_BASE_URL")
 IMAGE_BASE_URL = os.getenv("IMAGE_BASE_URL")
 PDF_NAME = "artifact.pdf"
 SERVICE_NAME = "UDC_PDFmaker"
+
 # ログの設定
-LOG_FILE_NAME = "udc_pdfmaker.log"
+LOG_FILE_NAME = f"{SERVICE_NAME.lower()}.log"
 handler = logging.handlers.RotatingFileHandler(
     filename=LOG_FILE_NAME,
     encoding="utf-8",
@@ -36,7 +37,7 @@ handler = logging.handlers.RotatingFileHandler(
     backupCount=5,
 )
 discord.utils.setup_logging(level=logging.INFO, handler=handler)
-bot_logger = logging.getLogger("PDFmaker")
+bot_logger = logging.getLogger(SERVICE_NAME)
 
 
 async def write_log_message(message: str, category: str):
