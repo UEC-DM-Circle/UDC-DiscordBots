e7e58ea582b55d14c577341b8518e9d40c1463d0
Gitudon <oniku.udon.029@gmail.com>
2026-05-03 19:14:57 +0900

    Announce: メンション追加

--- UDC_Announce/bot/announce.py
+++ UDC_Announce/bot/announce.py
@@ -47,7 +47,7 @@ class Announce:
                 "SELECT id FROM announcements WHERE date BETWEEN CURDATE() + INTERVAL (8 - DAYOFWEEK(CURDATE())) DAY AND CURDATE() + INTERVAL (14 - DAYOFWEEK(CURDATE())) DAY AND is_announced = 0"
             )
             if not next_week_announcements:
-                message = "来週の予定がありません。\n教室の確保及び予定の新規追加をお願いします！"
+                message = "@everyone\n来週の予定がありません。\n教室の確保及び予定の新規追加をお願いします！"
                 await board_member_channel.send(message)
                 await alert_channel.send(message)
 
